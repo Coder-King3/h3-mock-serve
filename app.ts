0a53ab528429ab660bc01fd1a0f9d8ef197887f7
Coder-King3 <2196592083@qq.com>
2025-03-29 01:56:16 +0800

    build apis

--- app.ts
+++ app.ts
@@ -6,14 +6,14 @@ export const app = defineApplication({
   h3Options: {
     onError: errorHandler
   },
-  headers: {
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Allow-Headers':
-      'Accept, Authorization, Content-Length, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-CSRF-TOKEN, X-Requested-With',
-    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Expose-Headers': '*'
-  },
+  // headers: {
+  //   'Access-Control-Allow-Credentials': 'true',
+  //   'Access-Control-Allow-Headers':
+  //     'Accept, Authorization, Content-Length, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-CSRF-TOKEN, X-Requested-With',
+  //   'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Expose-Headers': '*'
+  // },
   prefix: '/api',
   routes
 })
--- middleware/header.ts
+++ middleware/header.ts
@@ -1,10 +1,12 @@
 import type { Middleware } from '../types'
 
-import { defineEventHandler, setHeaders } from 'h3'
+import { defineEventHandler, getHeaders, setHeaders } from 'h3'
 
 const headerMiddleware: Middleware<[Record<string, string>]> = (headers) => {
   return defineEventHandler((event) => {
     setHeaders(event, headers)
+
+    console.log('headers', getHeaders(event))
   })
 }
 
